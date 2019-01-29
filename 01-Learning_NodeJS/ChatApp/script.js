1f08c25d7ee6c949a65dbd0e284181a02eb81999
Gustavo Brunetto <gustavobruneto@gmail.com>
2019-01-29 13:45:34 -0800

    finish initial script to html

--- 01-Learning_NodeJS/ChatApp/script.js
+++ 01-Learning_NodeJS/ChatApp/script.js
@@ -1,6 +1,21 @@
 const message = document.getElementById('input-message');
+const messageList = document.getElementById('messages-list');
+
+const checkMessage = () => {
+    if (messageList.innerHTML === '') {
+        messageList.innerHTML = '<div class="message list-empty">No messages :(</div>';
+    }
+};
 
 const sendMessage = () => {
-    console.log(message.value);
-    
+    const msg = message.value;
+    if (messageList.innerHTML === '') {
+        messageList.innerHTML = `<div class="message">${msg}</div>`;
+    } else {
+        messageList.innerHTML += `<div class="message">${msg}</div>`;
+    }
+    message.value = '';
 };
+
+messageList.innerHTML = '';
+checkMessage();
