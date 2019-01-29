2d151a45acf21f891de0c8a46c61506bbc96cc06
Gustavo Brunetto <gustavobruneto@gmail.com>
2019-01-29 13:07:33 -0800

    Initial commit. Chat app from the first lesson started

---
+++ 01-Learning_NodeJS/ChatApp/server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+// create an instance of express
+const app = express();
+
+// using a static file
+app.use(express.static(__dirname));
+
+// start the server and listen for incoming requests
+const PORT = 3000;
+const server = app.listen(PORT, () => {
+    console.log('server is listening on port ', server.address().port);
+});
